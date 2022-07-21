faa7e6b539fe6d25689f328b1e5841eb8dfc1347
MugenAkmal <md.akmal260896@gmail.com>
2022-07-21 22:29:31 +0800

    add archives controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -52,7 +52,7 @@ class EntriesController < ApplicationController
     @entry.destroy
 
     respond_to do |format|
-      format.html { redirect_to entries_url, notice: "Entry was successfully destroyed." }
+      format.html { redirect_to entries_url, status: :see_other, notice: "Entry was successfully destroyed." }
       format.json { head :no_content }
     end
   end
