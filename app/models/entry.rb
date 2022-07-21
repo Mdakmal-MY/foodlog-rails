c8f14047ecbdb568972a5bf85382ce96fcbab22b
MugenAkmal <md.akmal260896@gmail.com>
2022-07-21 22:38:58 +0800

    add validation entry form

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,6 @@
 class Entry < ApplicationRecord
 
+    validates :calories, :proteins, :carbohydrates, :fats, :meal_type, presence: true
     def day
         self.created_at.strftime("%b %e, %y")
     end
